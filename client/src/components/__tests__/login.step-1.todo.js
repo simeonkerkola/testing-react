461cd555ca0cb6fde51b14603da0504386adacff
Simeon Kerkola <simeon.kerkola@gmail.com>
2019-10-23 14:07:16 +0300

    Test login

--- client/src/components/__tests__/login.step-1.todo.js
+++ client/src/components/__tests__/login.step-1.todo.js
@@ -1,35 +1,34 @@
 // Basic unit test
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from '../login'
+
 test('calls onSubmit with the username and password when submitted', () => {
   // Arrange
   // 🐨 create a fake object to hold the form field values (username and password)
+  const fields = {username: 'name', password: 'pass'}
+
   // 🐨 create a jest.fn() for your submit handler
+  const handleSubmit = jest.fn()
+
   // 🐨 render the Login component to a div
-  // 💰 const div = document.createElement('div')
-  //
+  const div = document.createElement('div')
+  ReactDOM.render(<Login onSubmit={handleSubmit} />, div)
+
   // 🐨 get the field nodes
-  // 💰 const inputs = div.querySelectorAll('input')
-  // 💰 const form = div.querySelector('form')
+  const inputs = div.querySelectorAll('input')
+  const form = div.querySelector('form')
+
   // 🐨 fill in the field values
-  //
+  inputs[0].value = fields.username
+  inputs[1].value = fields.password
+
   // Act
   // 🐨 submit the form:
-  // 💰 formNode.dispatchEvent(new window.Event('submit'))
+  form.dispatchEvent(new window.Event('submit'))
   //
   // Assert
   // 🐨 ensure your submit handler was called properly
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
+  expect(handleSubmit).toHaveBeenCalledWith(fields)
 })
-
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=login.step-1&em=
-*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(submitted).toBe(true)
-})
-////////////////////////////////
